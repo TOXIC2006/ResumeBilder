8ca664ebc543d02ecabacf0152a4d62c222f0629
Siddharth_Rawat <sidraw2006@gmail.com>
2026-04-18 02:19:20 +0530

    uploadimage

--- src/main/java/com/resume/builder/reume_builder/controler/Authcontroller.java
+++ src/main/java/com/resume/builder/reume_builder/controler/Authcontroller.java
@@ -3,14 +3,16 @@ package com.resume.builder.reume_builder.controler;
 import com.resume.builder.reume_builder.Dto.Authresponese;
 import com.resume.builder.reume_builder.Dto.RegisterRequest;
 import com.resume.builder.reume_builder.service.AuthService;
+import com.resume.builder.reume_builder.service.FileUploadservice;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Map;
 
 import static com.resume.builder.reume_builder.util.Appconstrains.*;
@@ -21,24 +23,25 @@ import static com.resume.builder.reume_builder.util.Appconstrains.*;
 @RequestMapping(Auth_contoller)
 public class Authcontroller {
 
-    private final AuthService authService;
+  private final AuthService authService;
+  private final FileUploadservice fileUploadservice;
 
+  @PostMapping(Register_Emial)
+  public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest registerRequest) {
 
-     @PostMapping(Register_Emial)
-    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest registerRequest) {
+    Authresponese authresponese = authService.register(registerRequest);
+    log.info("user registered successfully with email: {}", registerRequest.getEmail());
+    return ResponseEntity.status(HttpStatus.CREATED).body(authresponese);
 
+  }
 
-               Authresponese authresponese= authService.register(registerRequest);
-               log.info("user registered successfully with email: {}", registerRequest.getEmail());
-                return ResponseEntity.status(HttpStatus.CREATED).body(authresponese);
+  @GetMapping(Verify_Email)
+  public ResponseEntity<?> verifyEmail(@RequestParam String token) {
+    // todo implement email verification logic
+    log.info("inside verify email endpoint with token: {}", token);
+    authService.verifyemail(token);
+    return ResponseEntity.status(HttpStatus.OK).body(Map.of("message", "email verified successfully"));
+  }
 
-     }
-        @GetMapping(Verify_Email)
-     public  ResponseEntity<?> verifyEmail(@RequestParam String token){
-          // todo implement email verification logic
-              log.info("inside verify email endpoint with token: {}", token);
-            authService.verifyemail(token);
-          return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","email verified successfully"));
-     }
 
 }
